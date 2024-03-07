7e7f130e83acc2fe1db00b8f839e88f71424afd2
Eduardo Fernandes <eduardofernandes21031@gmail.com>
2024-03-07 15:38:35 -0300

    input component created

---
+++ src/assets/types/inputProps.ts
@@ -0,0 +1,13 @@
+export interface InputProps {
+    name: string;
+    register: any;
+    placeholder?: string;
+    label?: string;
+    error?: boolean;
+    mask?: string;
+    type?: string;
+    maskPlaceholder?: string;
+    alwaysShowMask?: boolean;
+    value?: any;
+  }
+  
