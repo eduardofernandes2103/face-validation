38aa3bbb6bd667df6bd88f1527c8d8683367dfed
Claudia Dias <dev7@capitalempreendedor.com.br>
2024-03-19 16:16:13 -0300

    feat(hooks): use stepper hook created

---
+++ src/hooks/useStepper/index.tsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import { UseStepperProps } from '@/assets/types/useStepperProps';
+
+function useStepper({ maxSteps }: UseStepperProps) {
+  const stepInitialValue = 1;
+  const [step, setStep] = useState(stepInitialValue);
+  const [userDocument, setUserDocument] = useState('')
+
+  function nextStep() {
+    if (step < maxSteps) {
+      setStep(step + 1);
+    }
+
+    return;
+  }
+
+  function previousStep() {
+    if (step > stepInitialValue) {
+      setStep(step - 1);
+    }
+
+    return;
+  }
+
+  function resetStep() {
+    setStep(stepInitialValue);
+  }
+
+  return {
+    step,
+    nextStep,
+    previousStep,
+    resetStep,
+    userDocument,
+    setUserDocument
+  };
+}
+
+export default useStepper;
