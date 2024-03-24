ba6f32f7615f06bfdd247363fba3c9a1ca98bfb0
Claudia Dias <dev7@capitalempreendedor.com.br>
2024-03-24 15:38:54 -0300

    feat(userstep1): undefined error fixed

--- src/components/userSteps/UserStep1.tsx
+++ src/components/userSteps/UserStep1.tsx
@@ -2,12 +2,20 @@ import styles from './styles.module.scss';
 import Image from 'next/image';
 import LogoSphere from '../../assets/images/sphere_simbolo_branco.svg';
 import useStepper from '@/hooks/useStepper';
+import { useContext } from 'react';
+import { StepperContext } from '@/hooks/context/stepsContext';
 
 const UserStepOne = () => {
-  const { nextStep } = useStepper(1);
+  const stepperContext = useContext(StepperContext);
+
+  if (!stepperContext) {
+    console.log('stepperContext', stepperContext);
+    return null;
+  }
+  const { nextStep } = stepperContext;
 
   return(
-    <div className={styles.overlay} onClick={nextStep}>
+    <div className={styles.overlay} onClick={() => nextStep()}>
       <div className={styles.firstLayer}>
         <Image src={LogoSphere} alt='sphere-logo'/>
         <h3>Para validar sua cortesia clique aqui</h3>
