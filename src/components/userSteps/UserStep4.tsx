2bbada8b28cd1f40ed1da0be2632b97f97536e6e
Eduardo Fernandes <eduardofernandes21031@gmail.com>
2024-03-25 20:24:29 -0300

    step4 created

---
+++ src/components/userSteps/UserStep4.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import styles from './styles.module.scss';
+// import { UseStepper } from '@/providers/stepper';
+import Webcam from 'react-webcam';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCamera } from '@fortawesome/free-solid-svg-icons/faCamera';
+import Button from '../button';
+
+const UserStepFour = () => {
+//   const {nextStep} = UseStepper();
+
+  const webcamRef = React.useRef<any>(null);
+
+  const [capturedImage, setCapturedImage] = useState<string | null>(null);
+
+  const handleCapture = () => {
+    if (webcamRef.current) {
+      const imageSrc = webcamRef.current.getScreenshot();
+      setCapturedImage(imageSrc);
+    }
+  }
+
+  return(
+    <div>
+      <div style={{ width: '100vw', height: '100vh', position: 'relative', overflow: 'hidden', background: 'white'}}>
+      {capturedImage ? (
+        <div className={styles.capturedImageContainer}>
+          <img src={capturedImage} alt="Captured" style={{ width: '100%', objectFit: 'cover' }}/>
+          <div className={styles.buttonContainer}>
+            <Button isBorder onClick={() => setCapturedImage(null)}>Tentar novamente</Button>
+            <Button isDefault>Salvar foto</Button>
+          </div>
+        </div>
+      ):(
+        <>
+          <Webcam
+            onClick={() => handleCapture()}
+            audio={false}
+            ref={webcamRef}
+            mirrored={false}
+            style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+            screenshotFormat="image/png"
+          />
+          <FontAwesomeIcon icon={faCamera} size={'3x'} className={styles.captureIcon} color={'#ffffff'}  onClick={() => handleCapture()}/>
+        </>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default UserStepFour;
