f436fa22087a58902a162bce8bc72d28b5990962
Epichin <d.pena@hotmail.com>
2024-03-27 14:22:16 -0300

    fix(redirect-to-mobile): change the return if it is a desktop for mobile.

--- src/hooks/useMobile.ts
+++ src/hooks/useMobile.ts
@@ -1,16 +1,15 @@
 import { useEffect, useState } from "react"
 
 export const useMobile = () => {
-    const [isDesktop, setIsDesktop] = useState(false)
+    const [isMobile, setIsMobile] = useState(false)
     useEffect(() => {
         const userAgent = navigator.userAgent
-        console.log('UserAgent', userAgent)
         const isAndroid = /Android/.test(userAgent)
         const isIOS = /(iPhone|Macintosh)/.test(userAgent)
         const mobileWidth = window.matchMedia('(max-width: 1024px)').matches
         const isMobileDevice = isAndroid || isIOS || mobileWidth;
-        setIsDesktop(!isMobileDevice)
+        setIsMobile(isMobileDevice)
     }, [])
 
-    return isDesktop
+    return isMobile
 }
