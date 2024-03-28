8395a052d848be4015c9647b2c785467d2a99fc6
Claudia Dias <dev7@capitalempreendedor.com.br>
2024-03-28 11:16:23 -0300

    feat(producer-tickets): redirect to mobile added

--- src/pages/producer-tickets/index.tsx
+++ src/pages/producer-tickets/index.tsx
@@ -6,8 +6,11 @@ import Button from "@/components/button";
 import { faCircleCheck } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useRouter } from "next/router";
+import { useMobile } from "@/hooks/useMobile";
+import RedirectToMobile from "@/components/redirectToMobile";
 
 export default function ProducerTickets(){
+  const isMobile = useMobile();
   const router = useRouter();
 
   const [freeTickets, setFreeTickets] = useState<any>();
@@ -28,26 +31,32 @@ export default function ProducerTickets(){
   }, [])
   
   return(
-    <div className={styles.container}>
-      <div className={styles.content}>
-        <h2>Lista de cortesias</h2>
-        <span>Cortesias geradas: {freeTickets?.length}</span>
-        <div className={styles.buttonWrapper}>
-          <Button isDefault onClick={() => handleRedirectToCreateTickets()}>Gerar cortesia</Button>
-        </div>
-        {freeTickets && (
-          <div>
-            {freeTickets?.map((ticket: any, index: any) => (
-              <div key={index} className={styles.ticket}>
-                <p>{ticket.client_name}</p>
-                {ticket.is_approved === true && (
-                  <FontAwesomeIcon icon={faCircleCheck} className={styles.icon} />
-                )}
+    <>
+      {isMobile ? (
+        <div className={styles.container}>
+          <div className={styles.content}>
+            <h2>Lista de cortesias</h2>
+            <span>Cortesias geradas: {freeTickets?.length}</span>
+            <div className={styles.buttonWrapper}>
+              <Button isDefault onClick={() => handleRedirectToCreateTickets()}>Gerar cortesia</Button>
+            </div>
+            {freeTickets && (
+              <div>
+                {freeTickets?.map((ticket: any, index: any) => (
+                  <div key={index} className={styles.ticket}>
+                    <p>{ticket.client_name}</p>
+                    {ticket.is_approved === true && (
+                      <FontAwesomeIcon icon={faCircleCheck} className={styles.icon} />
+                    )}
+                  </div>
+                ))}
               </div>
-            ))}
+            )}
           </div>
-        )}
-      </div>
-    </div>
+        </div>
+      ) : (
+        <RedirectToMobile isDesktop />
+      )}
+    </>
   )
 }
