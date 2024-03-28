4cc0620962ddeeefd41d71b8139f632ade8c22b2
Claudia Dias <dev7@capitalempreendedor.com.br>
2024-03-28 11:32:06 -0300

    feat(validation-code): redirect to mobile added

--- src/pages/validation-code/[codeToken].tsx
+++ src/pages/validation-code/[codeToken].tsx
@@ -6,25 +6,34 @@ import UserStepTwo from "@/components/userSteps/UserStep2";
 import UserStepThree from "@/components/userSteps/UserStep3";
 import UserStepFour from "@/components/userSteps/UserStep4";
 import UserStepFive from "@/components/userSteps/UserStep5";
+import { useMobile } from "@/hooks/useMobile";
+import RedirectToMobile from "@/components/redirectToMobile";
 
 export default function CodeToken(){
+  const isMobile = useMobile();
   const router = useRouter();
   const codeToken = router.query.codeToken
   const { step, toastBehavior, setToastBehavior } = UseStepper()
 
   return(
     <>
-      <UserStepOne shouldRender={step === 1 ? true : false} />
-      <UserStepTwo shouldRender={step === 2 ? true : false} />
-      <UserStepThree shouldRender={step === 3 ? true : false} />
-      <UserStepFour shouldRender={step === 4 ? true : false} codeValidation={codeToken}/>
-      <UserStepFive shouldRender={step === 5 ? true : false} />
-      <Toast 
-        type="error"
-        toastTitle="Ops ..." 
-        shouldOpenToast={toastBehavior}
-        shouldCloseToast={() => setToastBehavior(false)} 
-      >A Selfie que você enviou não cumpre as nossas diretrizes, tente novamente!</Toast>
+      {(isMobile === true || isMobile === undefined) ? (
+        <>
+          <UserStepOne shouldRender={step === 1 ? true : false} />
+          <UserStepTwo shouldRender={step === 2 ? true : false} />
+          <UserStepThree shouldRender={step === 3 ? true : false} />
+          <UserStepFour shouldRender={step === 4 ? true : false} codeValidation={codeToken}/>
+          <UserStepFive shouldRender={step === 5 ? true : false} />
+          <Toast 
+            type="error"
+            toastTitle="Ops ..." 
+            shouldOpenToast={toastBehavior}
+            shouldCloseToast={() => setToastBehavior(false)} 
+          >A Selfie que você enviou não cumpre as nossas diretrizes, tente novamente!</Toast>
+        </>
+      ) : (
+        <RedirectToMobile isDesktop />
+      )}
     </>
   )
 }
